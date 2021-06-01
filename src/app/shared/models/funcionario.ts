b2f5af54d9e6db40af9024958257354f0a8365dc
marcelofelipesilva <marcelofelipesilva@outlook.com>
2021-06-01 10:59:17 -0300

    adicionando models

---
+++ src/app/shared/models/funcionario.ts
@@ -0,0 +1,10 @@
+export interface Funcionario{
+
+  idFuncionario : number;
+  nome :string;
+  matricula:number;
+  dataAdmissao: Date;
+  cargo: string;
+  detalhesFuncao:string;
+
+}
---
+++ src/app/shared/models/tarefa.ts
@@ -0,0 +1,7 @@
+export interface Tarefa{
+  idTarefa:number;
+  nome:string;
+  dataEntrega:Date;
+  descricaoTarefa:string;
+  status:string;
+}
