efd791d971e46f2a1a3f0f93f2fd4be23133d92c
marcelofelipesilva <marcelofelipesilva@outlook.com>
2021-06-01 19:50:06 -0300

    atualizando routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: '',
+    redirectTo: 'funcionario',
+    pathMatch: 'full'
+    
+},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
